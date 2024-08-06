6845b0b97443275735081dc40d67fe8f4dc94c0b
M R Simran <simranrout204@gmail.com>
2024-08-06 23:39:15 +0530

    RandomUserGenerator

--- script.js
+++ script.js
@@ -1,29 +1,92 @@
 
-document.addEventListener("DOMContentLoaded", generateUser);
+        var userName, userEmail, userLocation, userBirthday, userPhone, userPassword;
+        var iconpart = document.querySelectorAll(".material-symbols-outlined");
+        var image = document.getElementById("userImg");
+        var Name = document.getElementById("userName");
+        var desc = document.getElementById("userDesc");
+    
+        document.addEventListener("DOMContentLoaded", generateUser);
 
-function generateUser() {
-    fetch("https://randomuser.me/api/")
-        .then(response => response.json())
-        .then(data => {
-            const user = data.results[0];
-            document.getElementById("user-img").src = user.picture.large;
-            document.getElementById("name").innerText = `${user.name.first} ${user.name.last}`;
-            document.getElementById("email").innerText = user.email;
-            document.getElementById("location").innerText = `${user.location.city}, ${user.location.country}`;
-            document.getElementById("birthday").innerText = new Date(user.dob.date).toLocaleDateString();
-            document.getElementById("phone").innerText = ` ${user.phone}`;
+        function generateUser() {
+            fetch("https://randomuser.me/api/")
+                .then((response) => response.json())
+                .then((data) => {
+                    const user = data.results[0];
+                    image.src = user.picture.large;
+                    userName = `${user.name.first} ${user.name.last}`;
+                    userEmail = `${user.email}`;
+                    userLocation = `${user.location.street.number} ${user.location.street.name}`;
+                    userBirthday = `${user.dob.date} (${user.dob.age} years)`;
+                    userPhone = `${user.phone}`;
+                    userPassword = `${user.login.password}`;
+                    Name.innerText = "Hi, My name is";
+                    desc.innerText = userName;
+                    iconpart[0].style.color = "green";
+                });
+        }
 
-            document.getElementById("user-container").style.display = "block";
-        })
-        .catch((error) => {
-            console.error("Error fetching user:", error);
-        });
+        iconpart[0].addEventListener('mouseover', generateName);
+        iconpart[1].addEventListener('mouseover', generateEmail);
+        iconpart[2].addEventListener('mouseover', generateLocation);
+        iconpart[3].addEventListener('mouseover', generateBirthday);
+        iconpart[4].addEventListener('mouseover', generatePhone);
+        iconpart[5].addEventListener('mouseover', generatePassword);
 
-    const iconName = document.getElementById('icon-name');
-    const iconEmail = document.getElementById('icon-email');
-    const iconLocation = document.getElementById('icon-location');
-    const iconBirthday = document.getElementById('icon-birthday');
-    const iconPhone = document.getElementById('icon-phone');
-    const iconPassword = document.getElementById('icon-password');
-    const hoverData = document.getElementById('hover-data');   
-}
+
+
+
+
+        function generateEmail() {
+            Name.innerText = "My email address is";
+            desc.innerText = userEmail;
+            iconpart[1].style.color = "green";
+            iconpart[0].style.color = "gray";
+        }
+
+        function generateName() {
+            Name.innerText = "Hi, My name is";
+            desc.innerText = userName;
+            iconpart[0].style.color = "green";
+            iconpart[1].style.color = "gray";
+        }
+        function generateLocation(){
+            Name.innerText= "My address is";
+            desc.innerText = userLocation;
+            iconpart[0].style.color = "gray";
+            iconpart[1].style.color = "gray";
+            iconpart[2].style.color = "green";
+
+        }
+        function generateBirthday(){
+            Name.innerText= "My birthday is";
+            desc.innerText = userBirthday;
+            iconpart[0].style.color = "gray";
+            iconpart[1].style.color = "gray";
+            iconpart[2].style.color = "gray";
+            iconpart[3].style.color = "green";
+
+        }
+        function generatePhone(){
+            Name.innerText= "My phone number is";
+            desc.innerText = userPhone;
+            iconpart[0].style.color = "gray";
+            iconpart[1].style.color = "gray";
+            iconpart[2].style.color = "gray";
+            iconpart[3].style.color = "gray";
+            iconpart[4].style.color = "green";
+        }
+
+        function generatePassword(){
+            Name.innerText= "My password is";
+            desc.innerText = userPassword;
+            iconpart[0].style.color = "gray";
+            iconpart[1].style.color = "gray";
+            iconpart[2].style.color = "gray";
+            iconpart[3].style.color = "gray";
+            iconpart[4].style.color = "gray";
+            iconpart[5].style.color = "green";
+
+        }
+
+
+       
